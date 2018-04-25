65b4370adfd774f35c112a33ba0b3697b7fb7ec0
johndoe83104 <johndoe83104@gmail.com>
2018-04-25 21:43:39 +0300

    Update Elevons.ino

--- Elevons.ino
+++ Elevons.ino
@@ -11,8 +11,8 @@ Servo right;                                 // Create object for right servo
 
 word sensorValue1 = 0;                       // Read raw value for up/down motion for elevons
 word sensorValue2 = 0;                       // Read raw value for roll, left/right motion for elevons
-byte lift = 0;       
-byte reverseLift = 0;
+byte leftElevator = 0;       
+byte rightElevator = 0;
 byte roll = 0;        
 byte leftServo = 0;
 byte rightServo = 0;
@@ -29,17 +29,17 @@ void loop() {
   sensorValue1 = analogRead(A0);            // Read raw value from pot
   sensorValue2 = analogRead(A1);            // reatd raw value from pot
 
-  lift = map(sensorValue1, 108, 925, servoDownRange, servoUpRange);                         // Map raw values from pots to 50-120
+  leftElevator = map(sensorValue1, 108, 925, servoDownRange, servoUpRange);                         // Map raw values from pots to 50-120
   roll = map(sensorValue2, 164, 934, servoMinRange, servoMaxRange);                         // Map raw values from pots to 0-70
-  reverseLift = map(sensorValue1, 108, 925, servoUpRange, servoDownRange);
+  rightElevator = map(sensorValue1, 108, 925, servoUpRange, servoDownRange);
 
   rollLeft = constrain(roll, servoMidRange, servoMaxRange);
   rollLeft = map(rollLeft, servoMidRange, servoMaxRange, servoMinRange, servoMidRange);
   rollRight = constrain(roll, servoMinRange, servoMidRange);
   rollRight = map(rollRight, servoMinRange, servoMidRange, servoMidRange, servoMinRange);
 
-  leftServo = constrain((lift-rollLeft+rollRight), servoDownRange, servoUpRange);
-  rightServo = constrain((reverseLift+rollRight-rollLeft), servoDownRange, servoUpRange);
+  leftServo = constrain((leftElevator-rollLeft+rollRight), servoDownRange, servoUpRange);
+  rightServo = constrain((rightElevator+rollRight-rollLeft), servoDownRange, servoUpRange);
   
   left.write(leftServo);
   right.write(rightServo);    
